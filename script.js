46b5fac0b03178342b76c586683d49ba1b146e91
unknown <miguel.semedo.2@outlook.com>
2022-10-01 19:04:49 +0100

    Implement Volume Controls and Toggle Mute

--- script.js
+++ script.js
@@ -57,6 +57,49 @@ function setProgress(e) {
 
 // Volume Controls --------------------------- //
 
+let lastVolume = 1;
+
+// Mute
+function toggleMute() {
+	volumeIcon.className = '';
+	if (video.volume) {
+		lastVolume = video.volume;
+		video.volume = 0;
+		volumeIcon.classList.add('fas', 'fa-volume-mute');
+		volumeIcon.setAttribute('title', 'Unmute');
+		volumeBar.style.width = 0;
+	} else {
+		video.volume = lastVolume;
+		volumeIcon.classList.add('fas', 'fa-volume-up');
+		volumeIcon.setAttribute('title', 'Mute');
+		volumeBar.style.width = `${lastVolume * 100}%`;
+	}
+}
+
+// Volume Bar
+function changeVolume(e) {
+	let volume = e.offsetX / volumeRange.offsetWidth;
+	// Rounding volume up or down
+	if (volume < 0.1) {
+		volume = 0;
+	}
+	if (volume > 0.9) {
+		volume = 1;
+	}
+	volumeBar.style.width = `${volume * 100}%`;
+	video.volume = volume;
+	// Change icon depending on volume
+	volumeIcon.className = '';
+	if (volume > 0.7) {
+		volumeIcon.classList.add('fas', 'fa-volume-up');
+	} else if (volume < 0.7 && volume > 0) {
+		volumeIcon.classList.add('fas', 'fa-volume-down');
+	} else if (volume === 0) {
+		volumeIcon.classList.add('fas', 'fa-volume-off');
+	}
+	lastVolume = volume;
+}
+
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
@@ -67,3 +110,6 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
